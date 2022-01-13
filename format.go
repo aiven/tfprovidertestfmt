f5f97ebf383e75e6310ce99fa81dced6cb64b05c
Michael Hoffmann <michael.hoffmann@aiven.io>
2022-01-13 16:30:28 +0100

    wip: factor out formatting for testability

---
+++ format.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"strings"
+	"unicode"
+
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+func formatEmbeddedTerraformManifests(ctx context.Context, fset *token.FileSet, tf *tfexec.Terraform, content []byte) ([]byte, error) {
+	parsed, err := parser.ParseFile(fset, "src.go", bytes.NewReader(content), parser.ParseComments)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse content: %w", err)
+	}
+
+	var (
+		lastfmtErr error
+		stack      []ast.Node
+	)
+	ast.Inspect(parsed, func(n ast.Node) bool {
+		defer func() {
+			if n == nil {
+				// Done with node's children. Pop.
+				stack = stack[:len(stack)-1]
+			} else {
+				// Push the current node for children.
+				stack = append(stack, n)
+			}
+		}()
+
+		if n == nil || len(stack) == 0 {
+			return true
+		}
+
+		lit, ok := n.(*ast.BasicLit)
+		if !ok || lit.Kind != token.STRING {
+			return true
+		}
+		if len(lit.Value) == 0 || lit.Value[0] != '`' {
+			return true
+		}
+
+		val := strings.Trim(lit.Value, "`")
+		val = strings.ReplaceAll(val, "\t", "")
+
+		if !isTerraformManifest(val) {
+			return true
+		}
+
+		indentation := getExpectedIndentation(stack, fset)
+
+		if val, err := tf.FormatString(ctx, val); err != nil {
+			lastfmtErr = err
+		} else {
+			lit.Value = "`" + strings.TrimRightFunc(strings.ReplaceAll(val, "\n", "\n"+strings.Repeat("\t", indentation)), unicode.IsSpace) + "`"
+		}
+		return true
+
+	})
+
+	if lastfmtErr != nil {
+		return nil, fmt.Errorf("unable to format file: %w", lastfmtErr)
+	}
+
+	buf := new(bytes.Buffer)
+
+	if err = format.Node(buf, fset, parsed); err != nil {
+		return nil, fmt.Errorf("unable to write formatted output into buffer: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+func isTerraformManifest(s string) bool {
+	_, diag := hclparse.NewParser().ParseHCL([]byte(s), "cand.tf")
+	return !diag.HasErrors()
+}
+
+func getExpectedIndentation(stack []ast.Node, fset *token.FileSet) int {
+	if ret := lastReturnStatement(stack); ret != nil {
+		return fset.Position(ret.Pos()).Column
+	}
+	if ass := lastAssignmentStatement(stack); ass != nil {
+		return fset.Position(ass.Pos()).Column
+	}
+	return 0
+}
+
+func lastReturnStatement(stack []ast.Node) ast.Node {
+	for i := len(stack) - 1; i != 0; i-- {
+		switch stack[i].(type) {
+		case *ast.ReturnStmt:
+			return stack[i]
+		default:
+			continue
+		}
+	}
+	return nil
+}
+
+func lastAssignmentStatement(stack []ast.Node) ast.Node {
+	for i := len(stack) - 1; i != 0; i-- {
+		switch stack[i].(type) {
+		case *ast.AssignStmt:
+			return stack[i]
+		default:
+			continue
+		}
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module github.com/aiven/tfprovidertestfmt
 go 1.17
 
 require (
-	github.com/hashicorp/go-version v1.4.0
 	github.com/hashicorp/hc-install v0.3.1
+	github.com/hashicorp/hcl/v2 v2.11.1
 	github.com/hashicorp/terraform-exec v0.15.0
 )
 
 require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-version v1.4.0 // indirect
 	github.com/hashicorp/terraform-json v0.13.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/zclconf/go-cty v1.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 	golang.org/x/text v0.3.5 // indirect
